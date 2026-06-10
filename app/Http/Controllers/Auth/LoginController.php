34ce03b6230218e56b3cb734c28b019842f2fe7b
Arum Purbaningrum <arumpurbaningrum03@gmail.com>
2026-06-10 13:35:58 +0700

    adding middleware

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -28,4 +28,9 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
     }
+
+    public function username()
+    {
+        return 'nik';
+    }
 }
